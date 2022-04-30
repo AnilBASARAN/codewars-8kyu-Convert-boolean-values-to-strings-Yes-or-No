4669efd2b30723c43dc14c2efcef9f57e6763ff2
Anıl Başaran <35750219+AnilBASARAN@users.noreply.github.com>
2022-04-30 04:04:07 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+let boolToWord = ( bool ) => bool ? 'Yes' : 'No'
+
+
+/*
+
+8kyu  Convert boolean values to strings 'Yes' or 'No'.
+
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
